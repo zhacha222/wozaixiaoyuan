89e54adef3b232bbcab243d5e79669f7e139b703
渣茶 <86164114+zhacha222@users.noreply.github.com>
2022-07-14 22:01:26 +0800

    签到

---
+++ qd.js
@@ -0,0 +1,42 @@
+/**
+ 作者QQ:1483081359
+ 微信小程序：我在校园 签到
+ github仓库：  https://github.com/zhacha222/wozaixiaoyuan
+
+ 变量名称：wzxy
+ 变量值：   {
+            "username": "手机号",
+            "password": "密码",
+            "qd_location": "118.911429,64.376742",
+            "rjrb_answers": ["0","0"],
+            "rjrb_location": "118.911429,64.376742",
+            "jkdk_answers": ["0","无","1","0","36.2","没有","1","1","2"],
+            "jkdk_location": "118.911429,64.376742",
+            "mark": "用户昵称"
+            }
+
+
+ **将变量填入【环境变量】之中去！！！
+ 不要填在【配置文件】！不要填在【配置文件】！不要填在【配置文件】！填配置文件里会报错！
+
+ 关于变量值中各值的解释:
+ username —— 手机号
+ password —— 密码
+
+ qd_location —— 签到 的经纬度（qd.js)
+
+ rjrb_answers —— 日检日报 的 填空内容（rjrb.js）
+ rjrb_location —— 日检日报 的 经纬度（rjrb.js）
+
+ jkdk_answers —— 健康打卡 的 填空内容（jkdk.js）
+ jkdk_location —— 健康打卡 的 经纬度（jkdk.js）
+
+ mark —— 用户昵称（不一定要真名，随便填都行,便于自己区分）
+
+
+ cron: 0
+ */
+
+
+正在写···
+
