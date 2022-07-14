f0c68c3a192d00a2f9595552f53a3610b8988ff8
渣茶 <86164114+zhacha222@users.noreply.github.com>
2022-07-14 21:53:07 +0800

    健康打卡

--- jkdk.js
+++ jkdk.js
@@ -4,18 +4,35 @@
  微信小程序：我在校园 健康打卡
  github仓库：  https://github.com/zhacha222/wozaixiaoyuan
 
- 变量格式：export wzxy='{
+ 变量名称：wzxy
+ 变量值：   {
             "username": "手机号",
             "password": "密码",
-            "rjrb_location": "118.911429,64.376742",
+            "qd_location": "118.911429,64.376742",
             "rjrb_answers": ["0","0"],
-            "jkdk_location": "118.911429,64.376742",
+            "rjrb_location": "118.911429,64.376742",
             "jkdk_answers": ["0","无","1","0","36.2","没有","1","1","2"],
-            "mark": "打卡用户"
-            }'
+            "jkdk_location": "118.911429,64.376742",
+            "mark": "用户昵称"
+            }
 
- 多用户用`@`隔开
 
+ **将变量填入【环境变量】之中去！！！
+ 不要填在【配置文件】！不要填在【配置文件】！不要填在【配置文件】！填配置文件里会报错！
+
+ 关于变量值中各值的解释:
+ username —— 手机号
+ password —— 密码
+
+ qd_location —— 签到 的经纬度（qd.js)
+
+ rjrb_answers —— 日检日报 的 填空内容（rjrb.js）
+ rjrb_location —— 日检日报 的 经纬度（rjrb.js）
+
+ jkdk_answers —— 健康打卡 的 填空内容（jkdk.js）
+ jkdk_location —— 健康打卡 的 经纬度（jkdk.js）
+
+ mark —— 用户昵称（不一定要真名，随便填都行,便于自己区分）
 
 cron: 1 8 * * *
 
@@ -28,7 +45,7 @@ const request = require('request');
 const {log} = console;
 const Notify = 1; //0为关闭通知，1为打开通知,默认为1
 //////////////////////
-let scriptVersion = "1.0.1";
+let scriptVersion = "1.0.0";
 let scriptVersionLatest = '';
 
 //我在校园账号数据
