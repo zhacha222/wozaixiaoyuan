3cccf10b4d277871d936c6cf4b83378573220aae
渣茶 <86164114+zhacha222@users.noreply.github.com>
2022-09-28 15:26:33 +0800

    报备返校 1.0.2

--- wzxy_bbfx.js
+++ wzxy_bbfx.js
@@ -96,7 +96,7 @@ let state = '';
             log(`\n============ 当前版本：${scriptVersion}  最新版本：${scriptVersionLatest} ============`)
 
             if(scriptVersionLatest != scriptVersion){
-                log(`\n发现新版本,请拉库更新！\n${update_data}`)
+                log(`\n发现新版本,请及时拉库更新！\n${update_data}`)
             }
 
             log(`\n=================== 共找到 ${wzxyArr.length} 个账号 ===================`)
